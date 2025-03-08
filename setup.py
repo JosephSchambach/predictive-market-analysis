bca78b0ab0ae8e5cfb570b3991fe889a19ab5407
Joseph Schambach <joseph@vortechsolutions.com>
2025-03-08 06:32:54 -0500

    inrecementing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="predictive_ma",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(where='predictive_ma',exclude=['analysis_main_flow', 'analysis_main_flow.*']),
     package_dir={'': 'predictive_ma'},
 )
